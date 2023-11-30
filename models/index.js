fca6ecd6a06c50e5018328677a3f572c318801e5
Mirandad10 <dmiranda0005@gmail.com>
2023-11-29 23:16:43 -0800

    added models

---
+++ models/index.js
@@ -0,0 +1,43 @@
+
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+
+
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+})
+
+
+
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+})
+
+
+
+Product.belongsToMany(Tag, {
+    through: {
+        model: ProductTag,
+        unique: false
+    }
+})
+
+
+
+Tag.belongsToMany(Product, {
+    through: {
+        model: ProductTag,
+        unique: false
+    }
+})
+
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+};
